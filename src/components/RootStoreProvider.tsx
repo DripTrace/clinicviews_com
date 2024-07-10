8c4f89f572e5fc2a2553200a9e289d4ae5b7bce0
DripTrace <me@russellpalma.com>
2024-07-10 18:26:40 -0500

    fixing

--- src/components/RootStoreProvider.tsx
+++ src/components/RootStoreProvider.tsx
@@ -4,6 +4,8 @@
 import { domainStore } from "@/store/domainStore";
 import { useEffect } from "react";
 import { Provider as DomainProvider } from "react-redux";
+import NotificationExample from "./NotificationExample";
+import InstallPrompt from "./InstallPrompt";
 
 export default function StoreProvider({
     children,
@@ -17,6 +19,8 @@ export default function StoreProvider({
     return (
         <DomainProvider store={domainStore}>
             {/* <DomainContextInitializer /> */}
+            <NotificationExample />
+            <InstallPrompt />
             {children}
         </DomainProvider>
     );
