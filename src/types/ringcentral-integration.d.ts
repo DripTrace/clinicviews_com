2206d7bc50ae31afa7d92798b8b20778b8036b8b
DripTrace <rpalm@driptrace.io>
2024-09-16 21:32:56 -0700

    still

--- src/types/ringcentral-integration.d.ts
+++ src/types/ringcentral-integration.d.ts
@@ -2,7 +2,12 @@
 //     export function getTranslateFn(): any; // Update this based on actual type if you know it
 // }
 
+// declare module "@ringcentral-integration/utils" {
+//     const content: any;
+//     export = content;
+// }
+
 declare module "@ringcentral-integration/utils" {
-    const content: any;
-    export = content;
+    export function getTranslateFn(i18n: any): (...args: any[]) => string;
+    // Add other functions or types from the package as needed
 }
