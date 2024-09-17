3dfd28217e1438275036ab7726de1b877df783e9
DripTrace <rpalm@driptrace.io>
2024-09-17 15:21:33 -0700

    testing more src/dest pairs

--- next.config.mjs
+++ next.config.mjs
@@ -460,6 +460,22 @@ const nextConfig = {
                 source: "/favicon.ico",
                 destination: "/api/favicon",
             },
+            {
+                source: "/api/:path*",
+                destination: "/api/:path*",
+            },
+            {
+                source: "/api/llpmg/sip-handler",
+                destination: "/api/llpmg/sip-handler",
+            },
+            {
+                source: "/api/llpmg/sip-ws",
+                destination: "/api/llpmg/sip-handler",
+            },
+            {
+                source: "/llpmg/audio/:path*",
+                destination: "/public/llpmg/audio/:path*",
+            },
         ];
     },
     async headers() {
