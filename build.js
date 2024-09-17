44dba03dbeac6e8689f8fffa84ec244904e370ce
DripTrace <rpalm@driptrace.io>
2024-09-16 21:14:35 -0700

    removed @ringcentral-integration/utils

--- build.js
+++ build.js
@@ -35,40 +35,6 @@ const replacements = [
         search: 'licenseBanner = (react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("div", { className: "svc-creator__banner" }, react__WEBPACK_IMPORTED_MODULE_1___default.a.createElement("span", { className: "svc-creator__non-commercial-text", dangerouslySetInnerHTML: htmlValue })));',
         replace: "licenseBanner = null;",
     },
-    {
-        search: `import I18n from '@ringcentral-integration/i18n';
-import { getTranslateFn } from '@ringcentral-integration/utils';
-
-import type enUS from './en-US';
-// @ts-expect-error
-import loadLocale from './loadLocale';
-
-const i18n = new I18n<typeof enUS>(loadLocale);
-
-export const t = getTranslateFn(i18n);
-
-export type I18nKey = keyof typeof enUS;
-
-export default i18n;
-`,
-        replace: `import I18n from '@ringcentral-integration/i18n';
-
-import type enUS from './en-US';
-// @ts-expect-error
-import loadLocale from './loadLocale';
-
-// Initialize the I18n instance
-const i18n = new I18n<typeof enUS>(loadLocale);
-
-// Use the 'translate' method from the I18n instance (or another appropriate method)
-export const t = i18n.translate.bind(i18n);
-
-// Define the I18nKey type based on the enUS translation keys
-export type I18nKey = keyof typeof enUS;
-
-export default i18n;
-`,
-    },
 ];
 
 async function replaceInFile(filePath) {
