f19e6e52bc42564faa37e70fb6e806a9113e25ea
DripTrace <rpalm@driptrace.io>
2024-09-16 18:59:42 -0700

    fixing node_module errors

---
+++ src/types/ringcentral-integration.d.ts
@@ -0,0 +1,8 @@
+declare module "@ringcentral-integration/utils" {
+    export function getTranslateFn(): any; // Update this based on actual type if you know it
+}
+
+// declare module '@ringcentral-integration/utils' {
+//     const content: any;
+//     export = content;
+//   }
