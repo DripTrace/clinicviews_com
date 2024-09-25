b2a5d801487aca4acc6cb28e5b60ba68364cfbb9
DripTrace <rpalm@driptrace.io>
2024-09-25 15:04:05 -0700

    fixing imports again

--- src/app/amh/present/layout.tsx
+++ src/app/amh/present/layout.tsx
@@ -5,12 +5,12 @@ import { cn } from "@/lib/access-mentalhealth/access-mentalhealth-utils";
 // import { cn } from "@/lib/utils";
 
 const geistSans = localFont({
-    src: "../../fonts/GeistVF.woff",
+    src: "../../../fonts/GeistVF.woff",
     variable: "--font-geist-sans",
     weight: "100 900",
 });
 const geistMono = localFont({
-    src: "../../fonts/GeistMonoVF.woff",
+    src: "../../../fonts/GeistMonoVF.woff",
     variable: "--font-geist-mono",
     weight: "100 900",
 });
--- src/app/amh/test/page.tsx
+++ src/app/amh/test/page.tsx
@@ -1,14 +1,8 @@
-// pages/index.tsx
-// import AnimatedBackground from "@/components/AnimatedBackground";
-// import TestBackground from "@/components/TestBackground";
-
 import AnimatedBackground from "@/components/Access-MentalHealth/AnimatedBackground";
 
 const Home: React.FC = () => {
     return (
         <div className="relative overflow-hidden">
-            {/* <TestBackground /> */}
-            {/* Other content */}
             <AnimatedBackground />
         </div>
     );
