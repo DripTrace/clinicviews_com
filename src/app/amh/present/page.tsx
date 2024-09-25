0fdb4d879d89f64144e07d19e9e0191efce0f6a3
DripTrace <rpalm@driptrace.io>
2024-09-25 14:58:05 -0700

    fixing imports

--- src/app/amh/present/page.tsx
+++ src/app/amh/present/page.tsx
@@ -1,11 +1,11 @@
-import ItemComponent from "@/components/ItemComponent";
+import ItemComponent from "@/components/Access-MentalHealth/ItemComponent";
 
 const PresentPage = () => {
-	return (
-		<div>
-			<ItemComponent></ItemComponent>
-		</div>
-	);
+    return (
+        <div>
+            <ItemComponent></ItemComponent>
+        </div>
+    );
 };
 
 export default PresentPage;
--- src/app/amh/test/page.tsx
+++ src/app/amh/test/page.tsx
@@ -1,15 +1,17 @@
 // pages/index.tsx
-import AnimatedBackground from "@/components/AnimatedBackground";
+// import AnimatedBackground from "@/components/AnimatedBackground";
 // import TestBackground from "@/components/TestBackground";
 
+import AnimatedBackground from "@/components/Access-MentalHealth/AnimatedBackground";
+
 const Home: React.FC = () => {
-	return (
-		<div className="relative overflow-hidden">
-			{/* <TestBackground /> */}
-			{/* Other content */}
-			<AnimatedBackground />
-		</div>
-	);
+    return (
+        <div className="relative overflow-hidden">
+            {/* <TestBackground /> */}
+            {/* Other content */}
+            <AnimatedBackground />
+        </div>
+    );
 };
 
 export default Home;
